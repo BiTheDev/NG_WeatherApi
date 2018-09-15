bc8d3bb66cbde54c86417f48be6093adc4b2674f
BrianHo <Curry30@Brians-MacBook-Pro.local>
2018-09-14 17:42:26 -0700

    working version

--- weather/src/app/app.module.ts
+++ weather/src/app/app.module.ts
@@ -2,24 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SeattleComponent } from './seattle/seattle.component';
-import { SanJoseComponent } from './san-jose/san-jose.component';
-import { BurbankComponent } from './burbank/burbank.component';
-import { DallasComponent } from './dallas/dallas.component';
-import { WashingtonComponent } from './washington/washington.component';
-import { ChicagoComponent } from './chicago/chicago.component';
 import { HttpService } from './http.service';
 import { HttpClientModule } from '@angular/common/http';
 import { CityComponent } from './city/city.component';
 @NgModule({
   declarations: [
     AppComponent,
-    SeattleComponent,
-    SanJoseComponent,
-    BurbankComponent,
-    DallasComponent,
-    WashingtonComponent,
-    ChicagoComponent,
     CityComponent
   ],
   imports: [
--- weather/src/app/city/city.component.ts
+++ weather/src/app/city/city.component.ts
@@ -9,6 +9,7 @@ import { HttpService } from '../http.service';
 export class CityComponent implements OnInit {
   public City;
   name :String;
+  img : String;
   constructor(
     private _httpService: HttpService,
     private _route: ActivatedRoute,
@@ -18,42 +19,27 @@ export class CityComponent implements OnInit {
   ngOnInit() {
     this._route.params.subscribe((params: Params) => {
       this.name = params.name;
-      console.log(params)
+      console.log("name: " + this.name)
+      let city = this._httpService.getWeather(this.name);
+      city.subscribe(data =>{
+        console.log("City data");
+        this.City = data;
+        console.log( "data " + this.City);
   });
-  let city = this._httpService.getWeather(this.name);
-  city.subscribe(data =>{
-    console.log("City data");
-    this.City = data;
-    console.log(this.City);
-    
-    
+    if(this.name == "seattle"){
+      this.img ="https://www.seattleu.edu/media/homepage/images/SeattleSkyline-hp.jpg";
+    }else if(this.name == "san Jose"){
+      this.img = "https://i.ytimg.com/vi/car6ehhepxw/maxresdefault.jpg";
+    }else if(this.name == "burbank"){
+      this.img = "http://www.burbankca.gov/Home/ShowImage?id=2356&t=635073167899370000";
+    }else if(this.name == "dallas"){
+      this.img = "https://d2t1047w253zzm.cloudfront.net/blog/2017/11/dallas.jpeg";
+    }else if(this.name == "washington"){
+      this.img = "https://cdn.shopify.com/s/files/1/1831/5307/products/16528.jpg?v=1499703455";
+    }else if(this.name == "chicago"){
+      this.img = "https://media-cdn.tripadvisor.com/media/photo-s/07/72/27/a3/chicago.jpg";
+    }
   })
-  console.log(this.City);
   
-  }
-  ngOnChanges(){
-    this.seattle()
-    this.sanJose();
-  }
-  goHome(){
-    this._router.navigate(['/']);
-  }
-  seattle(){
-    this._router.navigate(['/seattle'])
-  }
-  sanJose(){
-    this._router.navigate(['/sanJose'])
-  }
-  burbank(){
-    this._router.navigate(['/burbank'])
-  }
-  dallas(){
-    this._router.navigate(['/dallas'])
-  }
-  washington(){
-    this._router.navigate(['/washington'])
-  }
-  chicago(){
-    this._router.navigate(['/chicago'])
   }
 }
